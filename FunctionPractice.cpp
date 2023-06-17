febe4699c5eb21877475b378afffd4c899032bac
Jacqueline166 <136536499+Jacqueline166@users.noreply.github.com>
2023-06-17 11:59:56 -0700

    Made an add all function

--- FunctionPractice.cpp
+++ FunctionPractice.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -8,11 +9,24 @@ int findMax(int num1, int num2) {
     } else if (num2>num1) {
         return num2;
     }
-   
+}
+
+int addAll(vector<int> nums) {
+    int total=0;
+    for (int i=0; i<nums.size(); i++) {
+        total=nums[i]+total;
+    }
+    return total;
 }
 
 int main() {
+    cout<<"findMax"<<endl;
     cout<<findMax(4, 5)<<endl;
     cout<<findMax(5, 4)<<endl;
+    cout<<"addAll"<<endl;
+    vector<int> vec1= {10, 23, 40};
+    vector<int> vec2= {10, 23, 40, 50, 7};
+    cout<<addAll(vec1)<<endl;
+    cout<<addAll(vec2)<<endl;
     return 0;
 }
