6e192020016e97dd7ef466c21284b85b074898a1
Jacqueline166 <136536499+Jacqueline166@users.noreply.github.com>
2023-06-20 16:57:08 -0700

    made a fToC function

--- FunctionPractice.cpp
+++ FunctionPractice.cpp
@@ -19,14 +19,48 @@ int addAll(vector<int> nums) {
     return total;
 }
 
+float fToC(float temperature) {
+    return (temperature-32)*5/9;
+}
+
+char calculateGrade(float grade) {
+    if (grade>=90) {
+        return 'A';
+    } else if (grade>=80){
+        return 'B';
+    } else if (grade>=70) {
+        return 'C';
+    } else if (grade>=60) {
+        return 'D';
+    } else {
+        return 'F';
+    }
+        
+}
+
 int main() {
     cout<<"findMax"<<endl;
     cout<<findMax(4, 5)<<endl;
     cout<<findMax(5, 4)<<endl;
+
     cout<<"addAll"<<endl;
     vector<int> vec1= {10, 23, 40};
     vector<int> vec2= {10, 23, 40, 50, 7};
     cout<<addAll(vec1)<<endl;
     cout<<addAll(vec2)<<endl;
+
+    cout<<"calculateGrade"<<endl;
+    cout<<calculateGrade(97)<<endl;
+    cout<<calculateGrade(82)<<endl;
+    cout<<calculateGrade(60.3)<<endl;
+    cout<<calculateGrade(79.9)<<endl;
+    cout<<calculateGrade(32)<<endl;
+
+    cout<<"fToC"<<endl;
+    cout<<fToC(140)<<endl;
+    cout<<fToC(76)<<endl;
+    cout<<fToC(89)<<endl;
+    cout<<fToC(111)<<endl;
+    cout<<fToC(44.4)<<endl;
     return 0;
 }
