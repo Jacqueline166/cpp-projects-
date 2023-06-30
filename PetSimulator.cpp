ce9279822682583afa70a5ce2c8febde7547a45c
npand08 <135080793+npand08@users.noreply.github.com>
2023-06-29 17:40:34 -0700

    brainstormed pet simulator ideas

---
+++ PetSimulator.cpp
@@ -0,0 +1,10 @@
+/*
+class: player
+ - attributes: stamina, pets, money, name
+ - methods (actions): 
+sub class: pet
+ - attributes: happiness, health, hunger, hygine
+ - methods: eat, sleep, 
+sub sub classes: dog, cat, hamster, fish
+
+*/
